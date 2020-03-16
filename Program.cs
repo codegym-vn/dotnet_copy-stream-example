2b56cc7ff65fdcde0ffcb2b9ae455c3316322ce0
The Nguyen Khac <the.nguyen@codegym.vn>
2020-03-16 11:10:55 +0700

    Copy bin file

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Practice_CopyBinFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Source file path:");
+            string source = Console.ReadLine();
+            Console.WriteLine("Enter destination file path: ");
+            string dest = Console.ReadLine();
+            try
+            {
+                StreamCopyFile(source, dest);
+                Console.WriteLine("File is successfully copied");
+            }
+            catch (System.Exception)
+            {
+                Console.Error.WriteLine("Invalid Source path or Destination path");
+            }
+        }
+
+        static void StreamCopyFile(string source, string dest)
+        {
+            StreamReader reader = null;
+            StreamWriter writer = null;
+            try
+            {
+                reader = new StreamReader(source);
+                writer = new StreamWriter(dest);
+                int length;
+                char[] buffer = new char[1024];
+                while((length = reader.Read(buffer)) > 0)
+                {
+                    writer.Write(buffer, 0, length);
+                }
+            }
+            catch (System.Exception)
+            {
+                Console.Error.WriteLine("Invalid input or output path");
+            }
+            finally
+            {
+                reader.Close();
+                writer.Close();
+            }
+        }
+    }
+}
